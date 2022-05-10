87c19e10be56f75057091f7179c0d932d3bf597b
MaxShady <max.shady1611@gmail.com>
2022-05-10 17:13:09 +0430

    Add Tailwind

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -3,16 +3,20 @@ import classes from "./NoteItem.module.css";
 import Tag from "./Tag";
 
 const NoteItem = () => {
-    return (
-        <section className={`${classes.noteItem} ${classes.yellow}`}>
-            <h3 className={classes.noteTitle}>Hey!</h3>
-            <p className={classes.noteText}>This is a test text!</p>
-            <div className={classes.extra}>
-                <Tag className={classes.tag} tag="Notes" />
-                <p className={classes.date}>Sun, 2 Apr</p>
-            </div>
-        </section>
-    )
+  return (
+    <section className="w-48 bg-[#333333] m-4 p-4 rounded-xl flex flex-col">
+      <h3 className="font-bold">Homework</h3>
+      <p className="text-sm text-[#9F9F9F] mt-2">
+        Find 10 videos about illustrations to share with classmates.
+      </p>
+      <div className="flex flex-row items-center mt-8">
+        <p className="text-[#9F9F9F] text-xs border-2 rounded-md border-[#9F9F9F] px-2 py-1">
+          Notes
+        </p>
+        <p className="text-[#9F9F9F] text-xs ml-8">Sun, 2 Apr</p>
+      </div>
+    </section>
+  );
 };
 
 export default NoteItem;
